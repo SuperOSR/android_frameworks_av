e7a981344f12b7ae91bbad5f18e8ddbea495fca3
Blerfont <blerfont@gmail.com>
2013-12-10 18:57:30 +0100

    Update Android.mk

--- media/libstagefright/wifi-display/Android.mk
+++ media/libstagefright/wifi-display/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SRC_FILES:= \
         source/RepeaterSource.cpp       \
         source/TSPacketizer.cpp         \
         source/WifiDisplaySource.cpp    \
-        VideoFormats.cpp                \
+        VideoFormats.cpp                
 
 ifeq ($(TARGET_BOARD_PLATFORM), fiber)
     LOCAL_CFLAGS += -DTARGET_BOARD_FIBER
@@ -21,13 +21,13 @@ ifeq ($(TARGET_BOARD_PLATFORM), fiber)
         wfdsink/RTPSource.cpp           \
         wfdsink/RTPJitterBuffer.cpp     \
         wfdsink/WfdSink.cpp             \
-        wfdsink/MiracastSink.cpp        \
+        wfdsink/MiracastSink.cpp        
 endif
 
 LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/av/media/libstagefright \
         $(TOP)/frameworks/native/include/media/openmax \
-        $(TOP)/frameworks/av/media/libstagefright/mpeg2ts \
+        $(TOP)/frameworks/av/media/libstagefright/mpeg2ts 
 
 LOCAL_SHARED_LIBRARIES:= \
         libbinder                       \
@@ -38,7 +38,7 @@ LOCAL_SHARED_LIBRARIES:= \
         libstagefright                  \
         libstagefright_foundation       \
         libui                           \
-        libutils                        \
+        libutils                        
 
 ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
 LOCAL_CFLAGS += -DBOARD_BRCM_WLAN
